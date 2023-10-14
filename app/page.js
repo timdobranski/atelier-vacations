e685ec17cd307f5ad26857709e71ce961a6518ff
Tim <timdobranski@gmail.com>
2023-10-14 13:21:55 -0700

    Refactored shards to Image component

--- app/page.js
+++ app/page.js
@@ -1,18 +1,19 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
+import palms from '../public/palms.png';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={`${styles.bottomDiv}`}>
-        <div className={`${styles.shard} ${styles.shard1}`}><div className={styles.img}></div></div>
-        <div className={`${styles.shard} ${styles.shard2}`}><div className={styles.img}></div></div>
-        <div className={`${styles.shard} ${styles.shard3}`}><div className={styles.img}></div></div>
-        <div className={`${styles.shard} ${styles.shard4}`}><div className={styles.img}></div></div>
-        <div className={`${styles.shard} ${styles.shard5}`}><div className={styles.img}></div></div>
-        <div className={`${styles.shard} ${styles.shard6}`}><div className={styles.img}></div></div>
-        <div className={`${styles.shard} ${styles.shard7}`}><div className={styles.img}></div></div>
-        <div className={`${styles.shard} ${styles.shard8}`}><div className={styles.img}></div></div>
+        <div className={`${styles.shard} ${styles.shard1}`}><Image className={styles.img} src={palms} alt='shard' priority='true'></Image></div>
+        <div className={`${styles.shard} ${styles.shard2}`}><Image className={styles.img} src={palms} alt='shard' priority='true'></Image></div>
+        <div className={`${styles.shard} ${styles.shard3}`}><Image className={styles.img} src={palms} alt='shard' priority='true'></Image></div>
+        <div className={`${styles.shard} ${styles.shard4}`}><Image className={styles.img} src={palms} alt='shard' priority='true'></Image></div>
+        <div className={`${styles.shard} ${styles.shard5}`}><Image className={styles.img} src={palms} alt='shard' priority='true'></Image></div>
+        <div className={`${styles.shard} ${styles.shard6}`}><Image className={styles.img} src={palms} alt='shard' priority='true'></Image></div>
+        <div className={`${styles.shard} ${styles.shard7}`}><Image className={styles.img} src={palms} alt='shard' priority='true'></Image></div>
+        <div className={`${styles.shard} ${styles.shard8}`}><Image className={styles.img} src={palms} alt='shard' priority='true'></Image></div>
       </div>
     </main>
   )
