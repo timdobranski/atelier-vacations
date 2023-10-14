1bf7ce246eba4cd05e5bc389f823bae2dde342ec
Tim <timdobranski@gmail.com>
2023-10-14 00:55:24 -0700

    Finished intro animation

--- app/page.js
+++ app/page.js
@@ -5,12 +5,13 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={`${styles.bottomDiv}`}>
+        <h1 id={styles.title}>PARKWAY ACADEMY</h1>
         <div className={`${styles.shard} ${styles.shard1}`}><div className={styles.img}></div></div>
         <div className={`${styles.shard} ${styles.shard2}`}><div className={styles.img}></div></div>
         <div className={`${styles.shard} ${styles.shard3}`}><div className={styles.img}></div></div>
         <div className={`${styles.shard} ${styles.shard4}`}><div className={styles.img}></div></div>
         <div className={`${styles.shard} ${styles.shard5}`}><div className={styles.img}></div></div>
-        <div className={`${styles.shard} ${styles.shard6}`}><div className={styles.img} id={styles.shard6img}></div></div>
+        <div className={`${styles.shard} ${styles.shard6}`}><div className={styles.img}></div></div>
         <div className={`${styles.shard} ${styles.shard7}`}><div className={styles.img}></div></div>
         <div className={`${styles.shard} ${styles.shard8}`}><div className={styles.img}></div></div>
       </div>
